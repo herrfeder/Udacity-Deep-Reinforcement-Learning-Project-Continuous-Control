c03e627fd0d2b85e051e235d558493fe8703d7cd
david <david.lassig.ext@bwi.de>
2023-10-16 12:59:35 +0200

    adding headless

---
+++ cc_headless.py
@@ -0,0 +1,67 @@
+from unityagents import UnityEnvironment
+import numpy as np
+import numpy as np
+from ddpg_agent import Agent
+from collections import deque
+import torch
+
+env = UnityEnvironment(file_name='Reacher_Linux/Reacher.x86_64')
+
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+
+# number of agents
+num_agents = len(env_info.agents)
+print('Number of agents:', num_agents)
+
+# size of each action
+action_size = brain.vector_action_space_size
+print('Size of each action:', action_size)
+
+# examine the state space 
+states = env_info.vector_observations
+state_size = states.shape[1]
+print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
+print('The state for the first agent looks like:', states[0])
+
+agent = Agent(state_size=state_size, action_size=action_size, random_seed=0)
+
+episodes = 10000
+timesteps = 3000
+scores = []
+scores_deque = deque(maxlen=100)
+
+for episode in range(1, episodes+1): 
+
+    env_info = env.reset(train_mode=True)[brain_name]
+    agent.reset()
+    state = env_info.vector_observations[0]            # get the current state
+    score = 0
+    
+    for t in range(timesteps):
+        action = agent.act(state)                      # select an action
+
+        env_info = env.step(action)[brain_name]        # send the action to the environment
+        next_state = env_info.vector_observations[0]   # get the next state
+        reward = env_info.rewards[0]                   # get the reward
+        done = env_info.local_done[0]                  # see if episode has finished
+        agent.step(state, action, reward, next_state, done) # take step with agent (including learning)
+        score += reward                                # update the score
+        state = next_state                             # roll over the state to next time step
+        if done:                                       # exit loop if episode finished
+            break
+
+        scores_deque.append(score)       # save most recent score
+        scores.append(score)             # save most recent score
+              
+        if episode % 10 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(episode, np.mean(scores_deque)))
+            torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
+            torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
+        
+        if np.mean(scores_deque)>=30.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(episode, np.mean(scores_deque)))
+            torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
+            torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
+            break
